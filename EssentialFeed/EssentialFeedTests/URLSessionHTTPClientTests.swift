31039b5b683f18e062af85980a940871b6c41181
Karl Mõttus <karl.mottus@gmail.com>
2024-12-08 13:56:35 +0200

    Refactor URLSession subclass mocking with protocol based mocking

--- EssentialFeed/EssentialFeedTests/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/URLSessionHTTPClientTests.swift
@@ -5,10 +5,20 @@
 import XCTest
 import EssentialFeed
 
+protocol HTTPSession {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTask
+}
+
+protocol HTTPSessionTask {
+    func resume()
+}
+
+
+
 class URLSessionHTTPClient {
-    private let session: URLSession
+    private let session: HTTPSession
     
-    init(session: URLSession) {
+    init(session: HTTPSession) {
         self.session = session
     }
     
@@ -25,7 +35,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         
     func test_getFromURL_resumeDataTaskWithURL() {
         let url = URL(string: "https://example.com")!
-        let session = URLSessionSpy()
+        let session = HTTPSessionSpy()
         let task = URLSessionDataTaskSpy()
         session.stub(url: url, task: task)
         
@@ -39,7 +49,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         let url = URL(string: "https://example.com")!
         let error = NSError(domain: "any error", code: 1)
-        let session = URLSessionSpy()
+        let session = HTTPSessionSpy()
         session.stub(url: url, error: error)
         
         let sut = URLSessionHTTPClient(session: session)
@@ -63,19 +73,19 @@ class URLSessionHTTPClientTests: XCTestCase {
     //MARK: - Helpers
     
     // From swift 5.5 further we need to use @unchecked Sendable to conform to swift new concurrency. While using @unchecked Sendable we also need to make sure that our decorated class will be thread-safe.
-    final private class URLSessionSpy: URLSession, @unchecked Sendable {
+    final private class HTTPSessionSpy: HTTPSession, @unchecked Sendable {
         private var stubs = [URL: Stub]()
         
         private struct Stub {
-            let task: URLSessionDataTask
+            let task: HTTPSessionTask
             let error: Error?
         }
         
-        func stub(url: URL, task: URLSessionDataTask = FakeURLSessionDataTask(), error: Error? = nil) {
+        func stub(url: URL, task: HTTPSessionTask = FakeURLSessionDataTask(), error: Error? = nil) {
             stubs[url] = Stub(task: task, error: error)
         }
         
-        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTask {
             
             guard let stub = stubs[url] else {
                 fatalError("Couldn't find stub for \(url)")
@@ -85,13 +95,13 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
     }
     
-    private class FakeURLSessionDataTask : URLSessionDataTask, @unchecked Sendable {
-        override func resume(){ }
+    private class FakeURLSessionDataTask : HTTPSessionTask, @unchecked Sendable {
+        func resume(){ }
     }
-    private class URLSessionDataTaskSpy: URLSessionDataTask, @unchecked Sendable {
+    private class URLSessionDataTaskSpy: HTTPSessionTask, @unchecked Sendable {
         var resumeCallCount = 0
         
-        override func resume() {
+         func resume() {
             resumeCallCount += 1
         }
     }
