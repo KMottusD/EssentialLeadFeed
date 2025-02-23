c2870a58a21638cee3944d0f397c202bcbe92c84
Karl Mõttus <karl.mottus@gmail.com>
2025-02-23 14:28:58 +0200

    Delete method deliver no errors when the cache is empty

--- EssentialFeed/EssentialFeedTests/Feed cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed cache/CoreDataFeedStoreTests.swift
@@ -9,7 +9,7 @@ import XCTest
 import EssentialFeed
 
 class CoreDataFeedStoreTests: XCTestCase {
-
+    
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
         expect(sut, toRetrieve: .empty)
@@ -27,12 +27,12 @@ class CoreDataFeedStoreTests: XCTestCase {
         let timestamp = Date()
         
         sut.insert(feed.local, timestamp: timestamp, completion: { _ in })
-
+        
         expect(sut, toRetrieve: .found(feed: feed.local, timestamp: timestamp))
         
     }
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-
+        
         let sut = makeSUT()
         let feed = uniqueImageFeed()
         let timestamp = Date()
@@ -43,7 +43,7 @@ class CoreDataFeedStoreTests: XCTestCase {
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
-
+        
         let sut = makeSUT()
         let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
         
@@ -54,23 +54,31 @@ class CoreDataFeedStoreTests: XCTestCase {
         let sut = makeSUT()
         _ = insert((uniqueImageFeed().local, Date()), to: sut)
         let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
-
+        
         XCTAssertNil(insertionError, "Expected to override cache successfully")
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
         let sut = makeSUT()
         _ = insert((uniqueImageFeed().local, Date()), to: sut)
-
+        
         let latestFeed = [LocalFeedImage]()
         let latestTimestamp = Date()
         _ = insert((latestFeed, latestTimestamp), to: sut)
-
+        
         expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
     }
     
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
+        let deletionError = deleteCache(from: sut)
+        
+        XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
+        
+    }
+    
     // - MARK: Helpers
-
+    
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataFeedStore {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
         let storeURL = URL(fileURLWithPath: "/dev/null")
@@ -83,27 +91,27 @@ class CoreDataFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
     }
-
+    
     func expect(_ sut: FeedStore, toRetrieve expectedResult: RetrieveCachedFeedResult, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "Wait for cache retrieval")
-
+        
         sut.retrieve { retrievedResult in
             switch (expectedResult, retrievedResult) {
             case (.empty, .empty),
-                 (.failure, .failure):
+                (.failure, .failure):
                 break
-
+                
             case let (.found(expectedFeed, expectedTimestamp), .found(retrievedFeed, retrievedTimestamp)):
                 XCTAssertEqual(retrievedFeed, expectedFeed, file: file, line: line)
                 XCTAssertEqual(retrievedTimestamp, expectedTimestamp, file: file, line: line)
-
+                
             default:
                 XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
             }
-
+            
             exp.fulfill()
         }
-
+        
         wait(for: [exp], timeout: 1.0)
     }
     
@@ -117,6 +125,17 @@ class CoreDataFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         return insertionError
     }
-
-
+    
+    @discardableResult
+    func deleteCache(from sut: FeedStore) -> Error? {
+        let exp = expectation(description: "Wait for cache deletion")
+        var deletionError: Error?
+        sut.deleteCacheFeed { receivedDeletionError in
+            deletionError = receivedDeletionError
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+        return deletionError
+        
+    }
 }
