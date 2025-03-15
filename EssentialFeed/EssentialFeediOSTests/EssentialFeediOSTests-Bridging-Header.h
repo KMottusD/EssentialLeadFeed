e58845f948471f3f35820cca78480b2b28d683a6
Karl Mõttus <karl.mottus@gmail.com>
2025-03-15 14:37:54 +0200

    Test 'FeedViewController' does not load feed on initiation. Seperate CI schemes for IOS and MacOS

---
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  EssentialFeediOSTests-Bridging-Header.h
+//  EssentialFeed
+//
+//  Created by KM on 15.03.2025.
+//
+
+#ifndef EssentialFeediOSTests_Bridging_Header_h
+#define EssentialFeediOSTests_Bridging_Header_h
+
+
+#endif /* EssentialFeediOSTests_Bridging_Header_h */
