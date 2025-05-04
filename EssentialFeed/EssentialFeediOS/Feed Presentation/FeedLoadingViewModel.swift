86268353c36cdfbe135c0bf70a233ef8ee965ecd
Karl Mõttus <karl.mottus@gmail.com>
2025-05-04 20:05:12 +0300

    Refactor: 'FeedImageViewModel' from 'class' solution toi 'struct' solution from previous lecture. Extracted 'FeedLoadingViewModel' and 'FeedViewModel' into seperate 'own' files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by KM on 04.05.2025.
+//
+
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel .swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel .swift
+//  EssentialFeediOS
+//
+//  Created by KM on 04.05.2025.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
