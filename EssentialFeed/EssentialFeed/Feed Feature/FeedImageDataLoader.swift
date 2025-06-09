2f31ff682abeda3063c69584a4687435cfe2b4b7
Karl Mõttus <karl.mottus@gmail.com>
2025-06-09 21:57:47 +0300

    Moved FeedImageDataLoader from 'EssentialFeediOS' module to generic 'EssentialFeed' module

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataLoader.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by KM on 09.06.2025.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+    
+}
