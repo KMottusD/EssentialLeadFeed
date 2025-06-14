4b03d500eae6a8380d503367af0cc4c3ccc91333
Karl Mõttus <karl.mottus@gmail.com>
2025-06-14 22:53:35 +0300

    Move/Extract cahce feed image data use case into new 'CacheFeedImageDataUseCaseTests' test class

---
+++ EssentialFeed/EssentialFeedTests/Feed cache/CacheFeedImageDataUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  CacheFeedImageDataUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by KM on 14.06.2025.
+//
+
+import XCTest
+import EssentialFeed
+
+class CacheFeedImageDataUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
+        let store = FeedImageDataStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+
+}
