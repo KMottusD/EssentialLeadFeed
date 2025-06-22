5f504151fe058cddc1644d9916841b3c9690697b
Karl Mõttus <karl.mottus@gmail.com>
2025-06-22 10:32:22 +0300

    Renaming to get the tracking back

---
+++ EssentialApp1/EssentialAppTests/EssentialAppTests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  EssentialAppTests-Bridging-Header.h
+//  EssentialApp
+//
+//  Created by KM on 21.06.2025.
+//
+
+#ifndef EssentialAppTests_Bridging_Header_h
+#define EssentialAppTests_Bridging_Header_h
+
+
+#endif /* EssentialAppTests_Bridging_Header_h */
