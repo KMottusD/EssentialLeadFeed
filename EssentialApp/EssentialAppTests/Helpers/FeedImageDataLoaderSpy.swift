d9301eb94b340b3a52b488e939bd4cee6a508756
Karl Mõttus <karl.mottus@gmail.com>
2025-07-22 22:27:35 +0300

    Refactor - Moved "FeedImageDataLoaderSpy" from test file to "helpers" into it own file

---
+++ EssentialApp/EssentialAppTests/Helpers/FeedImageDataLoaderSpy.swift
@@ -0,0 +1,39 @@
+//
+//  FeedImageDataLoaderSPY.swift
+//  EssentialAppTests
+//
+//  Created by KM on 22.07.2025.
+//
+
+import Foundation
+import EssentialFeed
+
+class FeedImageDataLoaderSpy: FeedImageDataLoader {
+    
+    private(set) var messages = [(url: URL, completion:  (FeedImageDataLoader.Result) -> Void)]()
+    
+    var loadedURLs: [URL] {
+        return messages.map { $0.url }
+    }
+    var cancelledURLs = [URL]()
+    
+    private struct Task: FeedImageDataLoaderTask {
+        let callback: () -> Void
+        func cancel() { callback() }
+    }
+    
+    func complete(with data: Data, at index: Int = 0) {
+        messages[index].completion(.success(data))
+    }
+    
+    func complete(with error: NSError, at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        messages.append((url,completion))
+        return Task { [weak self] in
+            self?.cancelledURLs.append(url)
+        }
+    }
+}
