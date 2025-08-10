e932a7eb4558cff1679836a91074c4b5c1379bc6
Karl Mõttus <karl.mottus@gmail.com>
2025-08-10 09:39:29 +0300

    Displays cached feed on kaunch when customer has no connectivity

--- EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
@@ -8,32 +8,21 @@
 import Foundation
 import EssentialFeed
 
-public class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
-        
-        let decoratee: FeedImageDataLoader
-        let imageCache: FeedImageDataCache
-        
-        private class TaskWrapper: FeedImageDataLoaderTask {
-            var wrapped: FeedImageDataLoaderTask?
-            
-            func cancel() {
-                wrapped?.cancel()
-            }
-        }
-        
-        public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
-            self.decoratee = decoratee
-            self.imageCache = cache
-        }
-        
-        public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-            let task = TaskWrapper()
-            task.wrapped = decoratee.loadImageData(from: url) { [weak self] result in
-                completion(result.map { imageData in
-                    self?.imageCache.saveIgnoreResult(imageData, for: url)
-                    return imageData
-                })
-            }
-            return task
+public final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoader
+    private let cache: FeedImageDataCache
+
+    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            completion(result.map { data in
+                self?.cache.save(data, for: url) { _ in }
+                return data
+            })
         }
     }
+}
